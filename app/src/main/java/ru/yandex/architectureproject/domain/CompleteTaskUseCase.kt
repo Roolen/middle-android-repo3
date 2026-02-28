9c3820effc03290e7e654f6f0324777cb457a81d
Artem <zhimovskij@yandex.ru>
2026-02-28 19:47:12 +0500

    work done

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -1,12 +1,16 @@
 package ru.yandex.architectureproject.domain
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.time.delay
 import ru.yandex.architectureproject.data.repository.TaskRepository
+import java.time.Duration
 
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
 ) {
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        // TODO: Здесь будет автоудаление задачи
+        delay(10_000)
+        repository.deleteTask(taskId)
     }
 }
